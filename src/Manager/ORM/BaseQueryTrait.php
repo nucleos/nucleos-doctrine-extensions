6a7137c2bb116b993bc411553a4066afeaf8ea7f
core23 <mail@core23.de>
2019-04-10 20:47:58 +0200

    [PATCH] Minor internal code refactoring

--- src/Manager/ORM/BaseQueryTrait.php
+++ src/Manager/ORM/BaseQueryTrait.php
@@ -33,32 +33,40 @@ trait BaseQueryTrait
                 $order = $defaultOrder;
             }
 
-            $fieldSpl = explode('.', $field);
-
-            if (\count($fieldSpl) > 2) {
-                throw new InvalidArgumentException(sprintf('The fieldname "%s" cannot contain more than one dot', $field));
-            }
-
-            $table = $defaultAlias;
-
-            // Map entity to table name
-            if (2 === \count($fieldSpl)) {
-                $table = $fieldSpl[0];
-                $field = $fieldSpl[1];
-
-                foreach ($aliasMapping as $k => $v) {
-                    if ($fieldSpl[0] === $k) {
-                        $table = $v;
-                        $field = $fieldSpl[1];
-
-                        break;
-                    }
-                }
-            }
-
-            $builder->addOrderBy($table.'.'.$field, $order);
+            $this->addOrderField($builder, $defaultAlias, $field, $order, $aliasMapping);
         }
 
         return $builder;
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @param string       $table
+     * @param string       $field
+     * @param string       $order
+     * @param array        $aliasMapping
+     */
+    private function addOrderField(QueryBuilder $builder, string $table, string $field, string $order, array $aliasMapping = []): void
+    {
+        $fieldSpl = explode('.', $field);
+
+        if (\count($fieldSpl) > 2) {
+            throw new InvalidArgumentException(sprintf('The fieldname "%s" cannot contain more than one dot', $field));
+        }
+
+        // Map entity to table name
+        if (2 === \count($fieldSpl)) {
+            [$table, $field] = $fieldSpl;
+
+            foreach ($aliasMapping as $k => $v) {
+                if ($fieldSpl[0] === $k) {
+                    $table = $v;
+
+                    break;
+                }
+            }
+        }
+
+        $builder->addOrderBy($table.'.'.$field, $order);
     }
 }
