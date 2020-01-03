15e489671ab5906de151c06b2c084ea71e500ba7
core23 <mail@core23.de>
2020-01-03 16:21:52 +0100

    Remove AbstractEntityManager

--- tests/Fixtures/DemoEntityManager.php
+++ tests/Fixtures/DemoEntityManager.php
@@ -11,31 +11,83 @@ declare(strict_types=1);
 
 namespace Core23\Doctrine\Tests\Fixtures;
 
-use Core23\Doctrine\Adapter\ORM\AbstractEntityManager;
+use Core23\Doctrine\Adapter\ORM\EntityManagerTrait;
+use Core23\Doctrine\Manager\ORM\BaseQueryTrait;
 use Core23\Doctrine\Manager\ORM\SearchQueryTrait;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Expr\Composite;
 use Doctrine\ORM\QueryBuilder;
+use RuntimeException;
 
-final class DemoEntityManager extends AbstractEntityManager
+final class DemoEntityManager
 {
     use SearchQueryTrait;
+    use EntityManagerTrait;
+    use BaseQueryTrait;
 
     /**
-     * @return EntityManager|QueryBuilder
+     * @var ManagerRegistry
      */
-    public function getQueryBuilder(string $alias, ?string $indexBy = null)
+    private $registry;
+
+    /**
+     * @var string
+     */
+    private $class;
+
+    public function __construct(string $class, ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+        $this->class    = $class;
+    }
+
+    public function getQueryBuilder(string $alias, ?string $indexBy = null): QueryBuilder
     {
         return $this->createQueryBuilder($alias, $indexBy);
     }
 
-    public function searchWhereQueryBuilder(QueryBuilder $qb, string $field, array $values, bool $strict = false): Composite
-    {
+    public function searchWhereQueryBuilder(
+        QueryBuilder $qb,
+        string $field,
+        array $values,
+        bool $strict = false
+    ): Composite {
         return $this->searchWhere($qb, $field, $values, $strict);
     }
 
-    public function addOrderToQueryBuilder(QueryBuilder $builder, array $sort, string $defaultEntity, array $aliasMapping = [], string $defaultOrder = 'asc'): QueryBuilder
-    {
+    public function addOrderToQueryBuilder(
+        QueryBuilder $builder,
+        array $sort,
+        string $defaultEntity,
+        array $aliasMapping = [],
+        string $defaultOrder = 'asc'
+    ): QueryBuilder {
         return $this->addOrder($builder, $sort, $defaultEntity, $aliasMapping, $defaultOrder);
+    }
+
+    protected function getRepository(): EntityRepository
+    {
+        return $this->getObjectManager()->getRepository($this->class);
+    }
+
+    private function getObjectManager(): ObjectManager
+    {
+        $manager = $this->registry->getManagerForClass($this->class);
+
+        if (!$manager) {
+            throw new RuntimeException(
+                sprintf(
+                    'Unable to find the mapping information for the class %s.'
+                    .' Please check the `auto_mapping` option'
+                    .' (http://symfony.com/doc/current/reference/configuration/doctrine.html#configuration-overview)'
+                    .' or add the bundle to the `mappings` section in the doctrine configuration.',
+                    $this->class
+                )
+            );
+        }
+
+        return $manager;
     }
 }
